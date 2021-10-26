b88e71347894eec1ad9fa369dcd562ddd3bd28d2
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-10-26 21:40:13 +0200

    animation update

--- InfiniteJumper/Systems/SpriteAnimationSystem.cs
+++ InfiniteJumper/Systems/SpriteAnimationSystem.cs
@@ -1,16 +1,41 @@
 ﻿using InfiniteJumper.Components;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Undine.Core;
+using Undine.MonoGame;
 
 namespace InfiniteJumper.Systems
 {
-    internal class SpriteAnimationSystem : UnifiedSystem<SpriteAnimationComponent>
+    internal class SpriteAnimationSystem : UnifiedSystem<TransformComponent, SpriteAnimationComponent, ColorComponent>
     {
-        public override void ProcessSingleEntity(int entityId, ref SpriteAnimationComponent t)
+        public SpriteBatch SpriteBatch { get; set; }
+
+        public override void ProcessSingleEntity(int entityId,
+            ref TransformComponent a,
+            ref SpriteAnimationComponent b,
+            ref ColorComponent c)
         {
-            throw new NotImplementedException();
+            //if (Sprite != null)
+            {
+                //if (entity.CollisionComponent != null)
+                //{
+                //    var a = entity.CollisionComponent.Box.Size.ToVector2() / 2;
+                //    SpriteBatch.Draw(this.Sprite.Texture,
+                //    entity.Position + a,
+                //    this.Sprite.SourceRectangle,
+                //    Color.White,
+                //    0,
+                //    a,
+                //    1,
+                //    (Microsoft.Xna.Framework.Graphics.SpriteEffects)entity.Direction, 0);
+                //}
+                //else
+                {
+                    SpriteBatch.Draw(b.CurrentFrame.Texture, a.Position, b.CurrentFrame.SourceRectangle, c.Color);
+                }
+            }
         }
     }
 }
