ca2ae75954484f02282c4f386ba1631ff5235d21
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-11-06 17:14:57 +0100

    added color animation

---
+++ InfiniteJumper/Components/ColorAnimationComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfiniteJumper.Components
+{
+    public struct ColorAnimationComponent
+    {
+    }
+}
---
+++ InfiniteJumper/Components/ColorAnimationSystem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfiniteJumper.Components
+{
+    public class ColorAnimationSystem
+    {
+    }
+}
