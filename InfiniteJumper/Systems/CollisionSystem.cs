dffe3041bcb0bdedf962c187044d0a57dd46fee4
Tomasz Cekało <tomasz.cekalo@gmail.com>
2022-04-01 22:09:42 +0200

    -

--- InfiniteJumper/Systems/CollisionSystem.cs
+++ InfiniteJumper/Systems/CollisionSystem.cs
@@ -56,6 +56,10 @@ namespace InfiniteJumper.Systems
                             //thisEntity.AnimationState = 0;
                         }
                     }
+                    else
+                    {
+                        ;
+                    }
                 }
             }
         }
