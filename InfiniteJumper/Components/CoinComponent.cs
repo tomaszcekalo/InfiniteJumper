4843b5212532297fa3f07d7eee351d8052af0bfb
Tomasz Cekało <tomasz.cekalo@gmail.com>
2022-12-09 13:47:39 +0100

    .

--- InfiniteJumper/Components/CoinComponent.cs
+++ InfiniteJumper/Components/CoinComponent.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace InfiniteJumper.Components
-{
-    internal class CoinComponent
-    {
-    }
-}
+//namespace InfiniteJumper.Components
+//{
+//    internal class CoinComponent
+//    {
+//    }
+//}
